6e74f3689a3a7f8620153eacf6aec3c3e5985283
make <make17@inbox.ru>
2024-01-19 23:22:19 +0300

    RT_1_create_main - Создан микросервис mainView - Созданы mainFront и mainBack

---
+++ mainView/mainBack/src/main/java/com/finView/mainBack/MainBackApplication.java
@@ -0,0 +1,13 @@
+package com.finView.mainBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainBackApplication.class, args);
+	}
+
+}
---
+++ mainView/mainBack/src/test/java/com/finView/mainBack/MainBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finView.mainBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
